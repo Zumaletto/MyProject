87fedca640184ed9b19b8195df4a3171e7001677
Olesya Kirillova <okirillova@baltbet.ru>
2022-08-15 20:06:20 +0300

    create new classes, method distance

---
+++ Point/src/main/java/ru/point/Main.java
@@ -0,0 +1,13 @@
+package ru.point;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point p1 = new Point(15,-12);
+        Point p2 = new Point(-15, 33);
+
+        double dist = Point.distance(p1,p2);
+        System.out.println("Расстояние между двумя точками = " + dist);
+    }
+
+}
--- Point/src/main/java/ru/point/Point.java
+++ Point/src/main/java/ru/point/Point.java
@@ -1,4 +1,16 @@
 package ru.point;
 
 public class Point {
+   double x, y;
+   public Point (double x, double y){
+       this.x = x;
+       this.y = y;
+   }
+    public static double distance(Point p1, Point p2) {
+
+        double x = p1.x - p2.x;
+        double y = p1.y - p2.y;
+        double dist = Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2)));
+        return dist;
+    }
 }
