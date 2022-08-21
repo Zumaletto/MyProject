de563c368ebecda93c4937e08e329d8706b334db
Olesya Kirillova <okirillova@baltbet.ru>
2022-08-21 20:13:29 +0300

    create class TestBase. refactored classes GroupCreationTests and GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,63 +1,17 @@
 package ru.stqa.pft.addressbook;
 
-
-import java.time.Duration;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class GroupDeletionTests {
-    private WebDriver wd;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-    }
+public class GroupDeletionTests extends TestBase{
 
     @Test
-    public void testNew() throws Exception {
-        wd.get("http://localhost/addressbook/group.php");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.xpath("//div[@id='content']/form/span[2]/input")).click();
-        wd.findElement(By.xpath("//input[@name='delete']")).click();
-        wd.findElement(By.linkText("group page")).click();
+    public void testGroupDeletion() throws Exception {
+        goToGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
     }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
 
 }
 
