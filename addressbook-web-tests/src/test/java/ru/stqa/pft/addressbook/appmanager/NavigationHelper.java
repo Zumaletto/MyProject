beef3de0c7779ea7f6611d7f86bac7192015c194
Olesya Kirillova <okirillova@baltbet.ru>
2022-08-31 15:43:03 +0300

    made verification and enforcement of preconditions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,17 +10,36 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (!isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
+
     public void gotoAddNewPage() {
-        click(By.linkText("add new"));
+        if (wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")){
+    return;
+        }
+            click(By.linkText("add new"));
     }
-    public void gotoHomePage(){click(By.linkText("home"));}
+
+    public void gotoHomePage() {
+        if (!isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("home"));
+    }
+
+    public void gotoAddNext() {
+
+        click(By.linkText("add next"));
+    }
+
     public void logout() {
         click(By.linkText("Logout"));
     }
-    public void gotoAddNext() {
-        click(By.linkText("add next"));
-    }
+
 
 }
