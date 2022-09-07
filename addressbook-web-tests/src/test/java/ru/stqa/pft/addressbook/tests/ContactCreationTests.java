cf76c91ef002ada13821953883b9bd70b2bc6002
Olesya Kirillova <okirillova@baltbet.ru>
2022-09-07 23:03:39 +0300

    added lambda

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -17,20 +16,14 @@ public class ContactCreationTests extends TestBase {
 
         app.getNavigationHelper().gotoAddNewPage();
         ContactData contact = new ContactData(
-                "Alena", "Klichko", "Litva", "+1234587", "123@mail.ru");
+                "Oleg", "Klichko", "Litva", "+1234587", "123@mail.ru");
         app.getContactHelper().createContact(contact);
         app.getContactHelper().returnToHomePage();
 
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for(ContactData c: after){
-            if (c.getId() > max){
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
