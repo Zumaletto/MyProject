b7faeef2f52466ee3c856bdffe17340c1f8d3b8c
Olesya Kirillova <okirillova@baltbet.ru>
2022-09-17 20:46:58 +0300

    create generate class for groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -15,10 +17,11 @@ public class ContactCreationTests extends TestBase {
         app.goTo().homePage();
         Contacts before = app.contact().all();
         app.goTo().addNewPage();
+        File photo = new File("src/test/resources/vi1.jpg");
         ContactData contact = new ContactData()
                 .withLastName("Petrov").withFirstName("Anton").withAddress("Riga, Tytyh2ki street,17, 78549")
                 .withHomeTel("+7 812 345 72 85").withMobileTel("+79217777777").withWorkTel("8(812)55-33-15")
-                .withEmail("123@mail.ru").withEmail2("qw@mail.ru").withEmail3("abv@mail.ru");
+                .withEmail("123@mail.ru").withEmail2("qw@mail.ru").withEmail3("abv@mail.ru").withPhoto(photo);
         app.contact().create(contact);
         app.contact().returnToHomePage();
         assertThat(app.contact().count(), equalTo(before.size() + 1));
@@ -26,6 +29,27 @@ public class ContactCreationTests extends TestBase {
 
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
+
+    @Test(enabled = true)
+    public void testAddNextContact() {
+        app.goTo().homePage();
+        Contacts before = app.contact().all();
+        app.goTo().addNewPage();
+        File photo = new File("src/test/resources/vi1.jpg");
+        ContactData contact = new ContactData()
+                .withLastName("Pupkin").withFirstName("Alex").withAddress("St.Peterburg")
+                .withHomeTel("+7(921)2-85").withMobileTel("+792177-77").withWorkTel("")
+                .withEmail("123@mail.ru").withEmail2("").withEmail3("").withPhoto(photo);;
+        app.contact().create(contact);
+        app.goTo().addNextContact();
+        ContactData contactNext = new ContactData()
+                .withLastName("Ivanova").withFirstName("Olga").withAddress("Riga")
+                .withHomeTel("+7(921)275-85-87").withMobileTel("+792177-77").withEmail("123@mail.ru");
+        app.contact().create(contactNext);
+        app.contact().returnToHomePage();
+        Contacts after = app.contact().all();
+        assertEquals(after.size(), before.size() + 2);
     }
 
     @Test(enabled = true)
@@ -42,26 +66,6 @@ public class ContactCreationTests extends TestBase {
         assertThat(app.contact().count(),equalTo(before.size()));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(before));
-
-    }
-
-    @Test(enabled = true)
-    public void testAddNextContact() {
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
-        app.goTo().addNewPage();
-        ContactData contact = new ContactData()
-                .withLastName("Pupkin").withFirstName("Alex").withAddress("St.Peterburg")
-                .withHomeTel("+7(921)2-85").withMobileTel("+792177-77").withEmail("123@mail.ru");
-        app.contact().create(contact);
-        app.goTo().addNextContact();
-        ContactData contactNext = new ContactData()
-                .withLastName("Ivanova").withFirstName("Olga").withAddress("Riga")
-                .withHomeTel("+7(921)275-85-87").withMobileTel("+792177-77").withEmail("123@mail.ru");
-        app.contact().create(contactNext);
-        app.contact().returnToHomePage();
-        Contacts after = app.contact().all();
-        assertEquals(after.size(), before.size() + 2);
 
     }
 
