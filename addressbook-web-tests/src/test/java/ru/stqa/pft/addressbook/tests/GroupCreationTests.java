2fcaf1c4388ceaaf903e0ef46c126838ac1c43f6
Olesya Kirillova <okirillova@baltbet.ru>
2022-09-18 16:45:08 +0300

    parametrized testBadGroupCreation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -14,7 +14,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-
     @DataProvider//провайдер тестовых данных
     public Iterator<Object[]> validGroups() {
         List<Object[]> list = new ArrayList<Object[]>();
@@ -23,6 +22,14 @@ public class GroupCreationTests extends TestBase {
         list.add(new Object[]{new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
         return list.iterator();
     } //итератор массивов объектов
+
+    @DataProvider//провайдер тестовых данных
+    public Iterator<Object[]> invalidGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("test1'").withHeader("header1").withFooter("footer1")});
+        return list.iterator();
+    }
+
 
     @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) throws Exception {
@@ -34,19 +41,16 @@ public class GroupCreationTests extends TestBase {
 
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
     }
 
-    @Test
-    public void testBadGroupCreation() throws Exception {
+    @Test(dataProvider = "invalidGroups")
+    public void testBadGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2'");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.group().all();
         assertThat(after, equalTo(before));
-
     }
 
 
