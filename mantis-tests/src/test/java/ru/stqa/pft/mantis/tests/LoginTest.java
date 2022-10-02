282772d777ee5c77de7bce51143085d26c910f8e
Olesya Kirillova <okirillova@baltbet.ru>
2022-10-02 20:19:25 +0300

    create new test class LoginTest

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTest.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.appmanager.HttpSession;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class LoginTest extends TestBase{
+    @Test
+    public void testLogin() throws Exception {
+        HttpSession session = app.newSession();
+        assertTrue(session.login("administrator","root"));
+        assertTrue(session.isLoggedInAs("administrator"));
+    }
+}
