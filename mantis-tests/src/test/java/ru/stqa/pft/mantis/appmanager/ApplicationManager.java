6b7a7ef21fd30f2c99b44fab4b91cc026bc04a1a
Olesya Kirillova <okirillova@baltbet.ru>
2022-10-08 17:14:22 +0300

    added changes by lesson 8.6

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
     private String browser;
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
+    private MailHelper mailHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -76,8 +77,14 @@ public class ApplicationManager {
 
             wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
             wd.get(properties.getProperty("web.baseUrl"));
-
         }
         return wd;
+    }
+
+    public MailHelper mail(){
+        if(mailHelper == null){
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
     }
 }
